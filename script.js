f3bc4d29188e97c8de69459e58d623e97820ce46
prit4848 <pritrajput415@gmail.com>
2024-08-05 22:21:48 +0530

    commit

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const userPoint = 0;
-const compPoint = 0;
+let userPoint = 0;
+let compPoint = 0;
 
 const choices = document.querySelectorAll(".choice")
+const messege = document.querySelector("#message")
+const userpoint = document.querySelector("#user-point")
+const comppoint = document.querySelector("#comp-point")
+const userOp = document.querySelector("#user-op")
+const compOp = document.querySelector("#comp-op")
 
 
 // use to get the user choice
@@ -10,18 +15,65 @@ choices.forEach((choice) =>{
         let userChoice = choice.getAttribute("id")
         playGame(userChoice)
     })
+
+   
 })
 
 //use to generate the computer choice
-const compChoice = () =>{
-    const comp = ["rock","paper","scissors"]
-    const random = Math.floor(Math.random()*3)
-    return comp[random]
+// Example compChoice function (you should define this as per your requirement)
+const getCompChoice = () => {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
 }
 
 //play Game
-const playGame = (userChoice) =>{
-    console.log("user choice:",userChoice)
-    const compchoice = compChoice()
-    console.log("computer choice:",compchoice)
+const playGame = (userChoice) => {
+    console.log("user choice:", userChoice);
+    userOp.innerHTML = userChoice
+    const compChoice = getCompChoice(); 
+    console.log("computer choice:", compChoice);
+    compOp.innerHTML = compChoice
+
+    if (userChoice === compChoice) {
+        drawGame();
+    } else {
+        let userWin = true;
+        if (userChoice === "rock") {
+            userWin = (compChoice === "paper" ? false : true);
+        } else if (userChoice === "paper") {
+            userWin = (compChoice === "scissors" ? false : true);
+        } else {
+            userWin = (compChoice === "rock" ? false : true);
+        }
+        youWin(userWin);
+    }
+};
+
+const drawGame = () => {
+    console.log("It's a draw!");
+    messege.classList.add ("draw")
+    messege.classList.remove("userwin")
+    messege.classList.remove("userlose")
+    messege.innerHTML = "It's a draw!"
+}
+
+const youWin = (win) => {
+    if (win) {
+        console.log("You win!");
+         messege.classList.add("userwin")
+         messege.classList.remove("userlose")
+         messege.classList.remove("draw")
+         messege.innerHTML = "You win!"
+         userPoint += 1;
+         userpoint.innerHTML = userPoint;
+    } else {
+        console.log("You lose!");
+         messege.classList.add("userlose")
+         messege.classList.remove("userwin")
+         messege.classList.remove("draw")
+         messege.innerHTML = "You lose!"
+         compPoint += 1;
+         comppoint.innerHTML = compPoint;
+    }
 }
